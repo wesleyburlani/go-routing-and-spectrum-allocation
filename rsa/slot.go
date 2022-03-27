48684f10b94da09f1bc678b3f1875066797259ca
wesleyburlani <wesleyburlani@gmail.com>
2022-03-26 21:21:57 -0300

    added stringfy function

--- rsa/slot.go
+++ rsa/slot.go
@@ -1,6 +1,11 @@
 package rsa
 
-import "github.com/wesleyburlani/go-routing-and-spectrum-allocation/graphs"
+import (
+	"strings"
+
+	"github.com/wesleyburlani/go-routing-and-spectrum-allocation/graphs"
+	"github.com/wesleyburlani/go-routing-and-spectrum-allocation/utils"
+)
 
 type Slot struct {
 	Edge       *graphs.Edge
@@ -12,4 +17,8 @@ func NewSlot(edge *graphs.Edge, availables []int) *Slot {
 		Edge:       edge,
 		Availables: availables,
 	}
+}
+
+func (s *Slot) String() string {
+	return "slot(" + s.Edge.GetId() + "): " + strings.Join(utils.IntSliceToStringSlice(s.Availables), ", ")
 }
