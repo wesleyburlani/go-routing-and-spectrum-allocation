867ec28e04e40461f5afc4ea64255a84b7d3ffcb
wesleyburlani <wesleyburlani@gmail.com>
2022-03-26 21:21:45 -0300

    add stringfy table

--- rsa/table.go
+++ rsa/table.go
@@ -1,8 +1,13 @@
 package rsa
 
 import (
+	"io/ioutil"
+	"os"
+
+	printTable "github.com/jedib0t/go-pretty/table"
 	"github.com/wesleyburlani/go-routing-and-spectrum-allocation/graphs"
 	"github.com/wesleyburlani/go-routing-and-spectrum-allocation/modulations"
+	"github.com/wesleyburlani/go-routing-and-spectrum-allocation/utils"
 )
 
 type TableData struct {
@@ -62,7 +67,42 @@ func (t *Table) AvailableSlots(graph *graphs.Graph, path *graphs.Path) []*Slot {
 				available = append(available, key)
 			}
 		}
-		availableSlots = append(availableSlots, NewSlot(edge, available))
+		edgeCopy := &graphs.Edge{}
+		utils.Copy(edge, edgeCopy)
+		availableSlots = append(availableSlots, NewSlot(edgeCopy, available))
 	}
 	return availableSlots
+}
+
+func (t *Table) String() string {
+	table := printTable.NewWriter()
+	table.SetOutputMirror(os.Stdout)
+	header := printTable.Row{"demand"}
+	for _, value := range t.Data {
+		for i := 0; i < len(value); i++ {
+			header = append(header, i)
+		}
+		break
+	}
+	table.AppendHeader(header)
+
+	for key, value := range t.Data {
+		row := printTable.Row{}
+		row = append(row, key)
+		for _, element := range value {
+			rowValue := ""
+			if len(element.Values) > 0 {
+				rowValue += element.Values[0]
+			} else {
+				rowValue += "0"
+			}
+			if element.ProtectionDemand {
+				rowValue += "*"
+			}
+			row = append(row, rowValue)
+		}
+		table.AppendRow(row)
+	}
+	table.SetOutputMirror(ioutil.Discard)
+	return "TABLE: \n" + table.Render()
 }
